b606549fc235cf98d3df5b923b1a00f5d1059366
Kyle Langille <kyle.a.langille@gmail.com>
2024-01-06 10:44:03 -0330

    scaffolding auth0

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { styled } from "styled-components";
 import Button from "./UI/Button";
 
 const Header = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Head>
       <Heading>ReciPZ</Heading>
       <nav>
         <NavList>
           <li>
-            <Button>Log in</Button>
+            <Button onClick={() => loginWithRedirect()}>Log in</Button>
           </li>
         </NavList>
       </nav>
